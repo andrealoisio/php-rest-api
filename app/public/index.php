aeac6edb55147ebc3c58154b2a8762a94851259c
Andre Aloisio Mendes <andrealoisio@gmail.com>
2022-06-02 12:00:50 -0300

    chore: sync with course class

--- app/public/index.php
+++ app/public/index.php
@@ -1,2 +1,25 @@
 <?php
-phpinfo();
+
+declare(strict_types=1);
+
+ini_set("display_errors", "On");
+
+require __DIR__ . "/vendor/autoload.php";
+
+$path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+
+$parts = explode("/", $path);
+
+$resource = $parts[2];
+
+$id = $parts[3] ?? null;
+
+if ($resource != "tasks") {
+    
+    http_response_code(404);
+    exit;
+}
+
+$controller = new TaskController;
+
+$controller->processRequest($_SERVER['REQUEST_METHOD'], $id);
