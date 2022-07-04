341576c286a506d350b6e4718313b87e290b9ab1
Andre Aloisio Mendes <andrealoisio@gmail.com>
2022-07-04 15:53:57 -0300

    add default error handling

--- app/src/ErrorHandler.php
+++ app/src/ErrorHandler.php
@@ -1,6 +1,11 @@
 <?php
 
 class ErrorHandler {
+
+  public static function handleError(int $error_number, string $error_message, string $file, $line) {
+    throw new ErrorException($error_message, 0, $error_number, $file, $line);
+  }
+
   public static function handleException(Throwable $exception): void {
     http_response_code(500);
     echo json_encode([
